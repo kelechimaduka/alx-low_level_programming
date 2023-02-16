180ae54b0d80181e957b3b87552e829358b1f6fa
kelechimaduka <kelechimaduka9@gmail.com>
2023-02-16 09:32:18 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : using puts instead of printf
+ *
+ * Return: Returns zero
+ */
+
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle, ");
+	return (0);
+}
