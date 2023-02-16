552c98d3fce12bfcd5aca49d9559a132a3bf49f6
kelechimaduka <kelechimaduka9@gmail.com>
2023-02-16 09:03:31 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main ()
+
+/*
+ * This code is to print a sentence and start
+ * a new line
+ * also aims to return zero
+*/
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
