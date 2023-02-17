5eb8a3bd285dd41e60dd86bec2d7be6aefac0d09
kelechimaduka <kelechimaduka9@gmail.com>
2023-02-17 13:30:53 -0800

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - As above stated
+ *
+ * Return: success please
+ */
+
+int main(void)
+
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = 0; num2 <= 9; num2++)
+		{
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+		}
+			if (num1 == num2)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+	}
+
+	return (0);
+
+}
