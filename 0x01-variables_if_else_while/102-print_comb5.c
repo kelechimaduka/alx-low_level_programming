eaca9f28473038592266085d2818a9c54649310d
kelechimaduka <kelechimaduka9@gmail.com>
2023-02-17 14:11:41 -0800

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,27 +9,21 @@
 int main(void)
 
 {
-	int num1, num2, num3, num4;
+	int num1, num2;
 
-	for (num1 = 0; num1 <= 9; num1++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (num2 = 0; num2 <= 9; num2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			for (num3 = 0; num3 <= 9; num3++)
-			{
-				for (num4 = 0; num4 <= 9; num4++)
-				{
-					putchar((num1 % 10) + '0');
-					putchar((num2 % 10) + '0');
-					putchar(' ');
-					putchar((num3 % 10) + '0');
-					putchar((num4 % 10) + '0');
-					if (num1 == 9 &&  num2 == 8 && num3 == 9 && num4 == 9)
-						continue;
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+			if (num1 == 98 &&  num2 == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 
