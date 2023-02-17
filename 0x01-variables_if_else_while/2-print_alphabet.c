b68f083144d2cfa8638f28b61b8318624172c8d5
kelechimaduka <kelechimaduka9@gmail.com>
2023-02-17 08:36:23 -0800

    grit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main -as seen
+ *
+ * Return: return success
+ */
+
+int main(void)
+
+{
+	char alph = 'abcdefghijklmnopqrstuvwxyz';
+
+	{
+	putchar(alph);
+	}
+
+	return (0);
+}
