86773c98da71c2cc3a2d4c9e801c56df8f9a4e31
kelechimaduka <kelechimaduka9@gmail.com>
2023-02-17 03:39:28 -0800

    for

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main -as seen
+ *
+ * Return: return success
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	for (n > 0)
+
+	{
+		printf("%d is positive\n", n);
+	}
+
+	for (n = 0)
+
+	{
+		printf("%d is zero\n", n);
+	}
+
+	for (n < 0)
+
+	{
+		printf("%d is negative\n", n);
+	}
+
+	return (0)
+}
+
