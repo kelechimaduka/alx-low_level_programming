e2de64f2d44f350812a4940a7d6256652ba75a70
kelechimaduka <kelechimaduka9@gmail.com>
2023-02-17 12:25:29 -0800

    grit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main -as seen
+ *
+ * Return: return success
+ */
+
+int main(void)
+
+{
+	char hpla;
+
+	for (hpla =  'z'; hpla >= 'a'; hpla--)
+	{
+	putchar(hpla);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
