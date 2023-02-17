bfe8568d79e9a5f03dff67a9f7378d8657a1b661
kelechimaduka <kelechimaduka9@gmail.com>
2023-02-17 08:55:02 -0800

    grit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main -as seen
+ *
+ * Return: return success
+ */
+
+int main(void)
+
+{
+	char alph;
+	char ALPH;
+
+	for (alph =  'a'; alph <= 'z'; alph++)
+	{
+	putchar(alph);
+	}
+	for (ALPH = 'A'; ALPH <= 'Z'; ALPH++)
+	{
+		putchar(ALPH);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
