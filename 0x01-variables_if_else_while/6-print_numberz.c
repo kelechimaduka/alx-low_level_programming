f0519ea5c765b4b4a3d39eaf6d50c1e92dad18fc
kelechimaduka <kelechimaduka9@gmail.com>
2023-02-17 12:19:57 -0800

    grit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main -as seen
+ *
+ * Return: return success
+ */
+
+int main(void)
+
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
