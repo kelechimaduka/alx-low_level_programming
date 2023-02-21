c06b476ea3f38a7339657836497a0ae505b2da78
kelechimaduka <kelechimaduka9@gmail.com>
2023-02-21 15:31:42 -0800

    faith

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,16 +9,18 @@
 void jack_bauer(void)
 {
 	int n = 0;
+
 	for (n = 0; n <= 23; n++)
 	{
 		int m = 0;
+
 		for (n = 0; n <= 59; n++)
 		{
-			_putchar(n / 10 + 48);
-			_putchar(n % 10 + 48);
-			_putchar(':');
 			_putchar(m / 10 + 48);
 			_putchar(m % 10 + 48);
+			_putchar(':');
+			_putchar(n / 10 + 48);
+			_putchar(n % 10 + 48);
 			_putchar('\n');
 		}
 	}
