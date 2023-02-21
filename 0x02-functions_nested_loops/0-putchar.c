df7dff87ce18abb89a07056514d7da84c68eaadb
kelechimaduka <kelechimaduka9@gmail.com>
2023-02-21 04:05:43 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the content of c
+ *
+ * main- to call functions
+ *
+ * @c : to be printed
+ *
+ * Description : to print _putchar
+ *
+ * Return: value 1.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * main- to call functions
+ *
+ * Return: Value val
+ */
+
+int main(void)
+{
+	char val = _putchar("_putchar");
+
+	return (val);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory explains declaration of functions and nested loops
