d82785e476cb0f7202af31ccf474c1dd65c2e96d
kelechimaduka <kelechimaduka9@gmail.com>
2023-02-23 11:50:17 -0800

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit - to print digits
+ *
+ * @c: integer
+ *
+ * Return: return zero
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
