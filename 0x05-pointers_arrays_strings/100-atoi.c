ea6b52ba5558ffe73401e6352e3844e3919e3d95
kelechimaduka <kelechimaduka9@gmail.com>
2023-02-27 13:54:16 -0800

    atoi_sample

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _atoi - converts string to integer
+ *
+ * @s: string
+ *
+ * Return: Success
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int num[];
+	int sign;
+	int x;
+
+	if (s[0] == '-')
+	{
+		sign = -1;
+
+		for (i = 1; s[i] != '\0'; i++)
+		{
+			num[i - 1] = s[i];
+		}
+		for (i = 0; num[i] <= 9; i++)
+		{
+			x = num[i];
+		}
+	return (sign * x);
+	}
+	else
+	{
+		return (x);
+	}
+}
