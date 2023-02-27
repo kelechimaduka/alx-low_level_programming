0c93dc598f2be00cf7043f1bea59d57c6a5de206
kelechimaduka <kelechimaduka9@gmail.com>
2023-02-27 12:53:46 -0800

    printarray

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,13 +16,11 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < n; i++)
+		printf("%d, ", a[i]);
+
 		if (i == (n - 1))
 		{
 			printf("%d", a[n - 1]);
 		}
-		else
-		{
-			printf("%d, ", a[i]);
-		}
-	_putchar('\n');
+		_putchar('\n');
 }
