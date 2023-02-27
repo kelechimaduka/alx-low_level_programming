a5ba00e22ee713495744f99bce7d9afa79f3e7c5
kelechimaduka <kelechimaduka9@gmail.com>
2023-02-27 11:08:43 -0800

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints reverse characters
+ *
+ * @s: string to be displayed
+ *
+ * Return: success please
+ */
+
+void print_rev(char *s)
+{
+	int i, j;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		count++
+	}
+
+	for (j = (count - 1); j >= 0; --j)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
