ec303e7e1c11e9560e5e90f21ca90029f6a9b66d
kelechimaduka <kelechimaduka9@gmail.com>
2023-02-27 13:06:12 -0800

    destination

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies string from source to destination
+ *
+ * dest: destination
+ *
+ * src: source
+ *
+ * Return: destination value
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
