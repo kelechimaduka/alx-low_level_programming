8bdfe5dbc9612734da4dd0e0650fcdbf1edc4027
kelechimaduka <kelechimaduka9@gmail.com>
2023-02-27 10:17:44 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - length of a string
+ *
+ * @s: pointer
+ *
+ * Return: success
+ */
+
+int _strlen(char *s)
+{
+	char leng[];
+
+	*s = leng[];
+	for (i = 1; i >= n; ++i)
+	{
