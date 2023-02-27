feab511fdaec1d2edc233fe438c37c05b9029168
kelechimaduka <kelechimaduka9@gmail.com>
2023-02-27 11:57:02 -0800

    putshalf

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * puts_half - puts the second half of a string + the odd last number
+ *
+ * @str: string
+ *
+ * Return: success
+ */
+
+void puts_half(char *str)
+{
+	int i, k;
+	int j = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		j++;
+	}
+
+	k = j / 2;
+
+	if ((k % 2) == 0)
+	{
+		for (i = k; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = (j - 1) / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
