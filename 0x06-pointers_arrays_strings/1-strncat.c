89d483142b2398fbaf5601f213a45d882c9bfe6b
kelechimaduka <kelechimaduka9@gmail.com>
2023-03-01 04:29:48 -0800

    conc

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strncat - conatenates teo strings
+ * 
+ * @dest: destination
+ * @src: source
+ * @n: integer concerned
+ *
+ * Return: Sucess value
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (j = 0; j < n; j++)
+	{
+		dest[i + j] = src[j];
+		if (j == '\0')
+		{
+			j = n;
+		}
+	}
+	return (dest);
+}
