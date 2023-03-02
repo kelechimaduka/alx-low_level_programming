870b64d3d9ac3e3d0469ce78fc5b117f9270f2cf
kelechimaduka <kelechimaduka9@gmail.com>
2023-03-02 15:23:01 -0800

    copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -19,10 +19,13 @@ char *_strncpy(char *dest, char *src, int n)
 		if (i < n)
 		{
 			dest[i] = src[i];
-		}
-		while (i < n)
+			while (i < n)
+			{
+				dest[i++] = '\0';
+			}
+		else
 		{
-			dest[i++] = '\0';
+			continue;
 		}
 	}
 
