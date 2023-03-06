0e793ed10ef052920d0b4d06316ddf6104d9b1ea
kelechimaduka <kelechimaduka9@gmail.com>
2023-03-06 09:27:34 -0800

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strspn - prints length of prefix substring
+ *
+ * @s: string
+ * @accept: string
+ *
+ * Return: Success
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, check, k;
+
+	k = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		check = 0;
+
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				k++;
+				check = 1;
+			}
+		}
+
+		if (check == 0)
+		{
+			return (k);
+		}
+	}
+	return(k);
+}
