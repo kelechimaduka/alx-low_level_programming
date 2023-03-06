0353a568a1649f9dbc4f2f91db93b8f110e031fc
kelechimaduka <kelechimaduka9@gmail.com>
2023-03-06 08:35:07 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ *
+ * @s: constant
+ * @b: constant byte
+ * @n: constant
+ *
+ * Return: Success
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for(i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a project for even more pointers, arrays and strings
