aa8588ec0a44d625304bbb6d1a564da52dfcd882
kelechimaduka <kelechimaduka9@gmail.com>
2023-03-14 08:44:41 -0700

    stati

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - main code
+ *
+ * Return: success
+ */
+
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the b
+			est hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}
