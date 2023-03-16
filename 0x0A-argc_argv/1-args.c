2134b65bb9a49eadeb27ec49d8c5afee6c249a70
kelechimaduka <kelechimaduka9@gmail.com>
2023-03-15 18:30:28 -0700

    arg2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main
+ *
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: success
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
