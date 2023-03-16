464e641a4db09d46fb9932371a29c79e65a3bc2a
kelechimaduka <kelechimaduka9@gmail.com>
2023-03-15 18:19:20 -0700

    arg1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main
+ *
+ * argc: argc
+ * argv: argv
+ *
+ * Return: success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
