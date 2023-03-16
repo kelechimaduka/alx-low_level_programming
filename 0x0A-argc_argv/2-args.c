bb5f7dfccb865762733a227778208bb2cc9d2640
kelechimaduka <kelechimaduka9@gmail.com>
2023-03-15 18:35:05 -0700

    arg3

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main
+ *
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+		return (0);
+	}
+}
