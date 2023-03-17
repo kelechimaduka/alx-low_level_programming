b845f1d77230f701c0dab523d22c131d8632617b
kelechimaduka <kelechimaduka9@gmail.com>
2023-03-17 05:12:46 -0700

    mal2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,13 +17,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, r1, r2, r3;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
+
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
+
 	for (r1 = 0; s1[r1] != '\0'; r1++)
 		;
 
@@ -31,29 +29,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		;
 
 	if (n > r2)
-	{
 		n = r2;
-	}
 
 	r3 = r1 + n;
 
 	t = malloc(r3 + 1);
 	if (t == NULL)
-	{
 		return (NULL);
-	}
 
-	for (i = 0; 1 < r3; i++)
-	{
+	for (i = 0; i < r3; i++)
 		if (i < r1)
-		{
 			t[i] = s1[i];
-		}
+
 		else
-		{
 			t[i] = s2[i - r1];
-		}
-	}
 
 	t[i] = '\0';
 
