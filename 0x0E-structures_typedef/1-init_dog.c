4d13ab89c158bba7667ec6740f8aa75cb0e7f4e7
kelechimaduka <kelechimaduka9@gmail.com>
2023-03-20 14:27:39 -0700

    initialize

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize variables
+ *
+ * @d: struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * 
+ * Return: success
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
