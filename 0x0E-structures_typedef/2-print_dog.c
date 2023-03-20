e5c73d6506450e9b05c6c2274338136450c79eb8
kelechimaduka <kelechimaduka9@gmail.com>
2023-03-20 14:44:03 -0700

    printdog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,43 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints the struct dog
+ *
+ * @d: struct
+ *
+ * Return: success
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (!(d->name))
+		{
+			printf("Name: (nil)\n");
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+		}
+
+		if (!(d->age))
+		{
+			printf("Age: (nil)\n");
+		}
+		else
+		{
+			printf("Age: %f\n", d->age);
+		}
+
+		if (!(d->owner))
+		{
+			printf("Owner: (nil)\n");
+		}
+		else
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+	}
+}
