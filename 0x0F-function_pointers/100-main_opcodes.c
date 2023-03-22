e22a48c1c15e211cbecad2156746b0e90d976692
kelechimaduka <kelechimaduka9@gmail.com>
2023-03-22 10:31:46 -0700

    :opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - maincode
+ *
+ * @argc: count
+ * @argv: vector
+ *
+ * Return: Success
+ */
+
+int main(int argc, char *argv[])
+{
+	char *hih = (char *) main;
+	int i, num;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	num = atoi(arg[1]);
+
+	if (num < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < num; i++)
+	{
+		printf("%02x", hih[i] & 0xFF);
+		if (i != num - 1)
+		{
+			printf(" ");
+		}
+	}
+
+	printf("\n");
+	return (0);
+}
