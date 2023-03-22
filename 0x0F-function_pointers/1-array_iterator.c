651b8d1cc97024690882aa0ea704192afca3670d
kelechimaduka <kelechimaduka9@gmail.com>
2023-03-22 09:12:22 -0700

    iterate

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "fiunction_pointers.h"
+
+/**
+ * srray_iterator - iterates array
+ *
+ * @array: array involved
+ * @size: size of array
+ * @action: function pointer
+ *
+ * Return: Success
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array && action)
+	{
+		for(i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
