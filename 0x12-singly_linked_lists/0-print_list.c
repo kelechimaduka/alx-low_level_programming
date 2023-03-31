41c6e80e4cc3f7b93f5b6b0dc76e309872e9196e
kelechimaduka <kelechimaduka9@gmail.com>
2023-03-31 03:10:48 -0700

    task00

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 	for (i = 0; h; i++)
 	{
 		printf("[%d] %s\n", h->len, h->str);
-		h = h->next_item;
+		h = h->next;
 	}
 	return (i);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -14,7 +14,7 @@ typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct list_s *next_item;
+	struct list_s *next;
 }
 list_t;
 
