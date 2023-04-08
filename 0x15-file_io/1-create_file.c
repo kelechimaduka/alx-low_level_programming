fea4eb00d8988a96842e115ad989a86fc2659a78
kelechimaduka <kelechimaduka9@gmail.com>
2023-04-08 14:19:14 -0700

    file io 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	new = open(filename, O-WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	new = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (new == -1)
 		return (-1);
 	if (text_content == NULL)
