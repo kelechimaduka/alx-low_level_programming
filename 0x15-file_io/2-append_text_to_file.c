2b6ba2bb9a180a1596e0bdd24967c04b9e92c034
kelechimaduka <kelechimaduka9@gmail.com>
2023-04-08 14:28:27 -0700

    file io 3

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,6 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (1);
 	for (leng = 0; text_content[leng]; leng++)
 		;
+	status = write(add, text_content, leng);
 	close(add);
 	return (status == -1 ? -1 : 1);
 }
