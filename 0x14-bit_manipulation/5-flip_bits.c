0e89c53544946735dc2c85ddab7accadd2725550
kelechimaduka <kelechimaduka9@gmail.com>
2023-04-08 12:42:00 -0700

    bit man 6

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits to be flipped to get anothe number
+ *
+ * @n: int
+ * @index: index
+ *
+ * Return: sucess
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, reset;
+
+	reset = 0;
+	for (i = 8 * sizeof(n) - 1; i >= 0; i--)
+		if (((n ^ m) >> i) & 1)
+			reset++;
+	return (reset);
+}
