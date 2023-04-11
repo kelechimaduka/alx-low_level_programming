0f269600ab8c842dea2ae76b2da8b130cb9b306c
kelechimaduka <kelechimaduka9@gmail.com>
2023-04-10 23:07:54 -0700

    bitman

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/*get_endianness - processor endianness
+ *
+ * Return: Success
+ */
+
+int get_endianness(void)
+{
+	int i;
+	char *j;
+
+	i = 1;
+	j = (char *)&i;
+	return ((int)j[0]);
+}
